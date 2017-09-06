8e2e5cd66de2c35bd25e8a4ee4f8bcaa50825968
marcoman <marcoman@10.89.149.156>
2017-09-06 09:58:37 +0800

    Added HelloWorld Class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("20366890 Marco");
+		System.out.println("20366890 Man Way Marco");
 	}
 
 }
